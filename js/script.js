752688cd28847dfd066a4d8d0674f636417e9fb6
StrixROX <pk1234pratyushk@gmail.com>
2020-10-15 02:29:32 +0530

    Completed first mockup of the portfolio

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
 $(() => {
 
 	enableTooltips();
-	animateHome();
+
+	//slide the banner in
+	$('.content').animate({ 'left': 0 }, 800, (e) => {
+		if($(window).innerWidth() > 760){
+			positionAvatar()
+		}
+	});
+
+	if($(window).innerWidth() > 760){
+		messageTypingThingy()
+	}
+
+	//some parallax
+	$(window).scroll(() => {
+		$('.landing-sec .bg').css({'transform': 'translateY(' + String($(window).scrollTop()*0.25) + 'px)'});
+	})
 
 })
 
@@ -12,22 +27,28 @@ function enableTooltips(){
 		const text = $(el).attr('data-tooltip-dn');
 		$(el).append('<span class="tooltip">' + text + '</span>');
 	})
+
+	$('[data-tooltip-up]').each((i, el) => {
+		const text = $(el).attr('data-tooltip-up');
+		$(el).append('<span class="tooltip">' + text + '</span>');
+	})
 }
 
-function animateHome(){
-	//slide the banner in
-
-	$('.content').animate({ 'left': 0 }, 600, 'swing', (e) => {
-		//then position the avatar msg bubble
+function positionAvatar(){
+	$('.avatar').animate({'opacity': 1}, 400, (e) => {
 
 		const avatarSectionWidth = $('.avatar').innerWidth();
 		$('.msg').css({
 			'right': avatarSectionWidth*0.40,
 			'opacity': 1
 		});
-	});
 
+	})
+}
+
+function messageTypingThingy(){
 	const attribs = ['Hi!', 'I am a web developer', 'I am a music ethusiast', 'I am excited to have you here!'];
+	const n = attribs.length;
 	const msgbox = $('.avatar .msg span').typist({
 		speed: 20,
 		cursor: false,
@@ -37,8 +58,8 @@ function animateHome(){
 	counter = 0;
 	setInterval(() => {
 		msgbox
-		.typistAdd(attribs[counter % attribs.length])
-		.typistPause(1000 + 3000*(counter % attribs.length == attribs.length - 1))
-		.typistRemove(attribs[counter++ % attribs.length].length)
+		.typistAdd(attribs[counter % n])
+		.typistPause(1500 + 2000*(counter % n == n - 1))
+		.typistRemove(attribs[counter++ % n].length)
 	}, 1000)
 }
